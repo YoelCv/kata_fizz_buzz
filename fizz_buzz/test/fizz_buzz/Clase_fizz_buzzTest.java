b579eda483bb9c789946a85b4c9707248c9d4c5c
Yoel Cobelas <Yoel21995@gmail.com>
2017-04-05 13:46:38 +0200

    Ultimos

--- fizz_buzz/test/fizz_buzz/Clase_fizz_buzzTest.java
+++ fizz_buzz/test/fizz_buzz/Clase_fizz_buzzTest.java
@@ -23,29 +23,47 @@ public class Clase_fizz_buzzTest {
         String result = fb.getfizz_buzz(1);
         assertEquals("1",result);
     }
-    @Test
+
     public void testSomeMethod2() {
         Clase_fizz_buzz fb = new Clase_fizz_buzz();
         String result = fb.getfizz_buzz(2);
         assertEquals("2",result);
     }
-    
-    @Test
+
     public void testSomeMethod3() {
         Clase_fizz_buzz fb = new Clase_fizz_buzz();
         String result = fb.getfizz_buzz(3);
         assertEquals("fizz",result);
     }
-
-    @Test
+    
     public void testSomeMethod4() {
         Clase_fizz_buzz fb = new Clase_fizz_buzz();
         String result = fb.getfizz_buzz(5);
         assertEquals("buzz",result);
     }
+    
     public void testSomeMethod5() {
         Clase_fizz_buzz fb = new Clase_fizz_buzz();
         String result = fb.getfizz_buzz(15);
         assertEquals("fizz buzz",result);
     }
+    
+    public void testSomeMethod6() {
+        Clase_fizz_buzz fb = new Clase_fizz_buzz();
+        String result = fb.getfizz_buzz(2);
+        assertEquals("pepe",result);
+    }
+    
+     public void testSomeMethod7() {
+        Clase_fizz_buzz fb = new Clase_fizz_buzz();
+        String result = fb.getfizz_buzz(10);
+        assertEquals("pepe buzz",result);
+    }
+     
+     public void testSomeMethod8() {
+        Clase_fizz_buzz fb = new Clase_fizz_buzz();
+        String result = fb.getfizz_buzz(30);
+        assertEquals("pepe buzz fizz",result);
+    }
+    
 }
